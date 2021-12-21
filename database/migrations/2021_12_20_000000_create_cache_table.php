c07d21bc63312eb42c34019755095b40ecd6ff78
Shuo Cui <cuidream@hotmail.com>
2021-12-21 13:05:55 +1100

    add code

---
+++ database/migrations/2021_12_20_000000_create_cache_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCacheTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::dropIfExists('caches');
+        Schema::create('caches', function (Blueprint $table) {
+          $table->id();
+          $table->string('key');
+          $table->text('value');
+          $table->integer('expiration');
+
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cache');
+    }
+}
